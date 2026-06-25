e6f270c73d50b1f9ae17add64971dd86b776abb2
Rajesh Ranjan <rrdba2026@gmail.com>
2026-06-25 19:53:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,17 +3,4 @@ resource "azurerm_resource_group" "myrg" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "myrg2" {
-  name     = "RGB-rg"
-  location = "West Europe"
-}
 
-resource "azurerm_resource_group" "myrg3" {
-  name     = "RGC-rg"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "myrg4" {
-  name     = "RGD-rg"
-  location = "West Europe"
-}
